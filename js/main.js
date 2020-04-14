c0fa55e697d02be78e3da6c873fb1590881e9496
karimb0 <karimbennis30@gmail.com>
2020-04-14 23:09:57 +0300

    сверстал шапку

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
+$(function () {
+  $(".mini-menu").click(function () {
+    if ($("nav ul").css("display") == "none") {
+      $("nav ul").slideDown();
+    } else {
+      $("nav ul").slideUp();
+    }
+  });
 });
